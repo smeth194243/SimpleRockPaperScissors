22046ef058a3ed91cc214af8a154608b43e698a1
smeth194243 <seth.r.m1@gmail.com>
2017-03-24 12:55:57 -0600

    IT WERKS OH EM GEE

--- src/rps/controller/RPSController.java
+++ src/rps/controller/RPSController.java
@@ -31,7 +31,69 @@ public class RPSController
 			
 			if(randomInt == 3)
 			{
+				if(userInput.equals("1"))
+				{
+					System.out.println("You chose Rock, and I chose Scissors. You win!");
+				}
 				
+				if(userInput.equals("2"))
+				{
+					System.out.println("You chose Paper, and I chose Scissors. I win!");
+				}
+				
+				if(userInput.equals("3"))
+				{
+					System.out.println("You chose Scissors, and I chose Scissors. Swordfight!");
+				}
+			}
+			
+			if(randomInt == 2)
+			{
+				if(userInput.equals("1"))
+				{
+					System.out.println("You chose Rock, and I chose Paper. I win!");
+				}
+				
+				if(userInput.equals("2"))
+				{
+					System.out.println("You chose Paper, and I chose Paper. Great minds think alike!");
+				}
+				
+				if(userInput.equals("3"))
+				{
+					System.out.println("You chose Scissors, and I chose Paper. Swordfight!");
+				}
+			}
+			
+			if(randomInt == 1)
+			{
+				if(userInput.equals("1"))
+				{
+					System.out.println("You chose Rock, and I chose Rock. Pebbles!");
+				}
+				
+				if(userInput.equals("2"))
+				{
+					System.out.println("You chose Paper, and I chose Rock. You win!");
+				}
+				
+				if(userInput.equals("3"))
+				{
+					System.out.println("You chose Scissors, and I chose Rock. I win!");
+				}
+			}
+			
+			System.out.println("Play again?\nYes or No");
+			
+			//String userInput2 = input.nextLine();
+			userInput = input.nextLine();
+			if(userInput.equalsIgnoreCase("Yes"))
+			{
+				start();
+			}
+			else
+			{
+				System.exit(0);
 			}
 		}
 	}
